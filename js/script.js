dac6949e271f5b65cea8dc1535529bd31c662687
Ahra Cho <choar816@postech.ac.kr>
2021-12-31 22:59:12 +0900

    Add Swiper.js

--- js/script.js
+++ js/script.js
@@ -13,3 +13,25 @@ const closeMenu = function () {
 
 iconMenu.onclick = openMenu;
 backBtn.onclick = closeMenu;
+
+// Swiper
+var mySwiper = new Swiper('.swiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+// $(document).ready(function () {
+//   //initialize swiper when document ready
+//   var mySwiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     direction: 'vertical',
+//     loop: true,
+//   });
+// });
