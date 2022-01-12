52a8df37cf2307e4c2dcb583ddd3062137944fee
Ahra Cho <choar816@postech.ac.kr>
2022-01-13 01:23:02 +0900

    Add: email check feature

--- js/script.js
+++ js/script.js
@@ -1,21 +1,18 @@
+const upBtn = document.querySelector('.btn-up');
+
 const header = document.querySelector('header');
 const iconMenu = header.querySelector('.icon-menu');
 const drawerMenu = header.querySelector('.sect-menu');
 const menuBackBtn = header.querySelector('.btn-menuback');
-const upBtn = document.querySelector('.btn-up');
 
 const subscribeForm = document.querySelector('.form-subscribe');
+const subscribeInput = subscribeForm.querySelector('input');
 const subscribeBtn = subscribeForm.querySelector('button');
+
 const modalSect = document.querySelector('.modal-thankyou');
 const modalBtn = modalSect.querySelector('button');
 
-const openMenu = function () {
-  drawerMenu.style.right = '0px';
-};
-const closeMenu = function () {
-  drawerMenu.style.right = '-290px';
-};
-
+// up button
 const upScroll = function () {
   window.scrollTo({
     top: 0,
@@ -23,6 +20,15 @@ const upScroll = function () {
   });
 };
 
+// menu
+const openMenu = function () {
+  drawerMenu.style.right = '0px';
+};
+const closeMenu = function () {
+  drawerMenu.style.right = '-290px';
+};
+
+// modal
 const showModal = function () {
   modalSect.style.display = 'block';
 };
@@ -30,8 +36,30 @@ const hideModal = function () {
   modalSect.style.display = 'none';
 };
 
+// check email
+const emailCheck = function (str) {
+  const reg_email = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+
+  if (!reg_email.test(str)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// subscribe
+const subscribeClick = function () {
+  if (emailCheck(subscribeInput.value)) {
+    showModal();
+  } else {
+    alert('이메일 수정 부탁');
+  }
+}
+
+
+upBtn.onclick = upScroll;
 iconMenu.onclick = openMenu;
 menuBackBtn.onclick = closeMenu;
-upBtn.onclick = upScroll;
-subscribeBtn.onclick = showModal;
+subscribeBtn.onclick = subscribeClick;
+// subscribeBtn.onclick = showModal;
 modalBtn.onclick = hideModal;
