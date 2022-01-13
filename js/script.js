fc1b62a61f9be5773dcd7d8f4ba9c286af2d26a9
Ahra Cho <choar816@postech.ac.kr>
2022-01-13 16:23:14 +0900

    Add: prettierrc

--- js/script.js
+++ js/script.js
@@ -1,61 +1,61 @@
-const upBtn = document.querySelector('.btn-up');
+const upBtn = document.querySelector(".btn-up");
 
-const header = document.querySelector('header');
-const iconMenu = header.querySelector('.icon-menu');
-const drawerMenu = header.querySelector('.sect-menu');
-const menuBackBtn = header.querySelector('.btn-menuback');
+const header = document.querySelector("header");
+const iconMenu = header.querySelector(".icon-menu");
+const drawerMenu = header.querySelector(".sect-menu");
+const menuBackBtn = header.querySelector(".btn-menuback");
 
-const subscribeForm = document.querySelector('.form-subscribe');
-const subscribeInput = subscribeForm.querySelector('input');
-const subscribeBtn = subscribeForm.querySelector('button');
+const subscribeForm = document.querySelector(".form-subscribe");
+const subscribeInput = subscribeForm.querySelector("input");
+const subscribeBtn = subscribeForm.querySelector("button");
 
-const modalSect = document.querySelector('.modal-thankyou');
-const modalBtn = modalSect.querySelector('button');
+const modalSect = document.querySelector(".modal-thankyou");
+const modalBtn = modalSect.querySelector("button");
 
 // up button
 const upScroll = function () {
   window.scrollTo({
     top: 0,
-    behavior: 'smooth'
+    behavior: "smooth",
   });
 };
 
 // menu
 const openMenu = function () {
-  drawerMenu.style.right = '0px';
+  drawerMenu.style.right = "0px";
 };
 const closeMenu = function () {
-  drawerMenu.style.right = '-290px';
+  drawerMenu.style.right = "-290px";
 };
 
 // modal
 const showModal = function () {
-  modalSect.style.display = 'block';
+  modalSect.style.display = "block";
 };
 const hideModal = function () {
-  modalSect.style.display = 'none';
+  modalSect.style.display = "none";
 };
 
 // check email
 const emailCheck = function (str) {
-  const reg_email = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+  const reg_email =
+    /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
 
   if (!reg_email.test(str)) {
     return false;
   } else {
     return true;
   }
-}
+};
 
 // subscribe
 const subscribeClick = function () {
   if (emailCheck(subscribeInput.value)) {
     showModal();
   } else {
-    alert('이메일 수정 부탁');
+    alert("이메일 수정 부탁");
   }
-}
-
+};
 
 upBtn.onclick = upScroll;
 iconMenu.onclick = openMenu;
